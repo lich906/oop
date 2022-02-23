ad96adccc40d90494c13d34bcdcd483a83066f3a
AndreyM <222mushkin@gmail.com>
2022-02-23 21:48:52 +0300

    [1-crypt] first workable version

--- labs/1-crypt/1-crypt.cpp
+++ labs/1-crypt/1-crypt.cpp
@@ -2,8 +2,14 @@
 #include <optional>
 #include <string>
 #include <fstream>
+#include <array>
 
-constexpr auto ARGUMENTS_COUNT = 4;
+
+typedef std::array<int, 8> BitmapArray;
+
+constexpr auto ARGUMENTS_COUNT = 5;
+constexpr BitmapArray CRYPT_MAP = { 2, 3, 4, 6, 7, 0, 1, 5 };
+constexpr BitmapArray DECRYPT_MAP = { 5, 6, 0, 1, 2, 7, 3, 4 };
 
 enum class Operation
 {
@@ -79,7 +85,8 @@ std::optional<Args> ParseArg(int argc, char* argv[])
 		return std::nullopt;
 	}
 
-	if (!(argv[1] == "crypt" || argv[1] == "decrypt"))
+	std::string opStr(argv[1]);
+	if (!(opStr == "crypt" || opStr == "decrypt"))
 	{
 		LogError("Error: Unknow operation. Available options: \"crypt\" or \"decrypt\".\n");
 		return std::nullopt;
@@ -93,12 +100,70 @@ std::optional<Args> ParseArg(int argc, char* argv[])
 	}
 
 	Args parsedArgs = {};
-	parsedArgs.operation = (argv[1] == "crypt") ? Operation::crypt : Operation::decrypt;
+	parsedArgs.operation = (opStr == "crypt") ? Operation::crypt : Operation::decrypt;
 	parsedArgs.inputFilePath = argv[2];
 	parsedArgs.outputFilePath = argv[3];
 	parsedArgs.cryptKey = cryptKey.value();
 
 	return parsedArgs;
+}
+
+void MoveBit(unsigned char srcByte, unsigned char& destByte, int srcPos, int destPos)
+{
+	destByte |= (((srcByte >> srcPos) & 1) << destPos);
+}
+
+unsigned char ShuffleByte(unsigned char byte, const BitmapArray& shufflingMap)
+{
+	unsigned char shuffledByte = 0;
+	for (int i = 0; i < 8; ++i)
+	{
+		MoveBit(byte, shuffledByte, i, shufflingMap[i]);
+	}
+	return shuffledByte;
+}
+
+unsigned char ProcessByte(unsigned char byte, unsigned char key, Operation operation)
+{
+	if (operation == Operation::crypt)
+	{
+		return ShuffleByte(byte ^ key, CRYPT_MAP);
+	}
+	else
+	{
+		return ShuffleByte(byte, DECRYPT_MAP) ^ key;
+	}
+}
+
+bool CheckStreams(std::istream& inStream, std::ostream& outStream)
+{
+	if (inStream.bad())
+	{
+		LogError("Failed to read data from file.\n");
+		return false;
+	}
+
+	if (!outStream.flush())
+	{
+		LogError("Failed to write data into file.\n");
+		return false;
+	}
+
+	return true;
+}
+
+bool ProcessEncryption(std::istream& in, std::ostream& out, unsigned char key, Operation operation)
+{
+	char byte;
+	while (in.get(byte))
+	{
+		if (!(out.put(ProcessByte(byte, key, operation))))
+		{
+			break;
+		}
+	}
+
+	return CheckStreams(in, out);
 }
 
 bool ValidateStreams(std::ifstream& inStream, std::ofstream& outStream)
@@ -133,4 +198,11 @@ int main(int argc, char* argv[])
 	{
 		return 1;
 	}
+
+	if (!ProcessEncryption(inputFile, outputFile, args->cryptKey, args->operation))
+	{
+		return 1;
+	}
+
+	return 0;
 }
