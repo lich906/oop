d50d19d31f260f9d80f36ecf90044ea55cdba93c
AndreyM <222mushkin@gmail.com>
2022-05-14 21:35:21 +0300

    [5-my-class] 100th commit anniversary!!! now base iterator constructors are protected to avoid creation of base class instance

--- labs/5-my-string/include/MyBaseIterator.h
+++ labs/5-my-string/include/MyBaseIterator.h
@@ -7,14 +7,14 @@ public:
 	using iterator_category = std::bidirectional_iterator_tag;
 	using difference_type = ptrdiff_t;
 
-	MyBaseIterator();
-	MyBaseIterator(char* data);
-
 	const char* data() const;
 
 	bool operator!=(const MyBaseIterator& other) const;
 	bool operator==(const MyBaseIterator& other) const;
 
 protected:
+	MyBaseIterator();
+	MyBaseIterator(char* data);
+
 	char* m_data;
 };
