ed8e8c065b102289118a44c7313d705146d58cf1
AndreyM <222mushkin@gmail.com>
2022-05-15 12:41:38 +0300

    [5-my-string] moved MyBaseIterator declaration and impl to MyIterator.h and MyIterator.cpp, respectively

--- labs/5-my-string/include/MyIterator.h
+++ labs/5-my-string/include/MyIterator.h
@@ -1,5 +1,23 @@
 #pragma once
-#include "MyBaseIterator.h"
+#include <iterator>
+
+class MyBaseIterator
+{
+public:
+	using iterator_category = std::bidirectional_iterator_tag;
+	using difference_type = ptrdiff_t;
+
+	const char* data() const;
+
+	bool operator!=(const MyBaseIterator& other) const;
+	bool operator==(const MyBaseIterator& other) const;
+
+protected:
+	MyBaseIterator();
+	MyBaseIterator(char* data);
+
+	char* m_data;
+};
 
 class MyIterator : public MyBaseIterator
 {
--- labs/5-my-string/src/MyIterator.cpp
+++ labs/5-my-string/src/MyIterator.cpp
@@ -1,5 +1,30 @@
 #include "MyIterator.h"
 
+MyBaseIterator::MyBaseIterator()
+	: m_data(nullptr)
+{
+}
+
+MyBaseIterator::MyBaseIterator(char* data)
+	: m_data(data)
+{
+}
+
+const char* MyBaseIterator::data() const
+{
+	return m_data;
+}
+
+bool MyBaseIterator::operator!=(const MyBaseIterator& other) const
+{
+	return m_data != other.m_data;
+}
+
+bool MyBaseIterator::operator==(const MyBaseIterator& other) const
+{
+	return !(*this != other);
+}
+
 MyConstIterator::MyConstIterator()
 	: MyBaseIterator()
 {
